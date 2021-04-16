f1496dc1ec35dc4f67e2bbb27209541f3b5909a7
zarnyao.tan <zarnyao.tan@shopee.com>
2021-04-17 00:53:30 +0800

    generated schema

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # graphql-go-tutorial
 Build a https://news.ycombinator.com clone with graphql and Golang.
+
+Link to tutorial: https://www.howtographql.com/graphql-go/1-getting-started/
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,21 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/zytan787/graphql-go-tutorial/graph/generated"
+	"github.com/zytan787/graphql-go-tutorial/graph/model"
+)
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
